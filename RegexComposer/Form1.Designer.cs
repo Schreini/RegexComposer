135726d53ce19985f75f3e4906422ec6fab3c7a2
Thomas Schreiner <t.schreiner@gmail.com>
2014-01-09 23:17:42 +0100

    Most basic matching works

--- RegexComposer/Form1.Designer.cs
+++ RegexComposer/Form1.Designer.cs
@@ -28,12 +28,74 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtRegex = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtText = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 8);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(38, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Regex";
+            // 
+            // txtRegex
+            // 
+            this.txtRegex.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtRegex.Location = new System.Drawing.Point(90, 5);
+            this.txtRegex.Name = "txtRegex";
+            this.txtRegex.Size = new System.Drawing.Size(889, 20);
+            this.txtRegex.TabIndex = 1;
+            this.txtRegex.TextChanged += new System.EventHandler(this.txtRegex_TextChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(13, 35);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(28, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Text";
+            // 
+            // txtText
+            // 
+            this.txtText.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtText.Location = new System.Drawing.Point(90, 32);
+            this.txtText.Multiline = true;
+            this.txtText.Name = "txtText";
+            this.txtText.Size = new System.Drawing.Size(889, 381);
+            this.txtText.TabIndex = 3;
+            this.txtText.TextChanged += new System.EventHandler(this.txtText_TextChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(991, 425);
+            this.Controls.Add(this.txtText);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtRegex);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtRegex;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtText;
     }
 }
 
--- RegexComposer/Form1.cs
+++ RegexComposer/Form1.cs
@@ -5,6 +5,7 @@ using System.Data;
 using System.Drawing;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Windows.Forms;
 
 namespace RegexComposer
@@ -15,5 +16,36 @@ namespace RegexComposer
         {
             InitializeComponent();
         }
+
+        private void txtRegex_TextChanged(object sender, EventArgs e)
+        {
+            MatchRegex();
+        }
+
+        private void txtText_TextChanged(object sender, EventArgs e)
+        {
+            MatchRegex();
+        }
+
+
+
+        private void MatchRegex()
+        {
+            try
+            {
+                var r = new Regex(txtRegex.Text);
+                bool isMatch = r.IsMatch(txtText.Text);
+                txtText.BackColor = isMatch ? Color.Green : Color.FromKnownColor(KnownColor.Window);
+                txtRegex.BackColor = Color.FromKnownColor(KnownColor.Window);
+            }
+            catch (Exception ex)
+            {
+                txtRegex.BackColor = Color.PaleVioletRed;
+            }
+        }
+
+
+
+
     }
 }
